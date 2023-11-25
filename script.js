830776087760e87499ffe3599f00ef3121bb802b
Aditi Lal <139861603+lal15aditi@users.noreply.github.com>
2023-11-25 18:02:51 +0530

    created a submission

--- script.js
+++ script.js
@@ -2,3 +2,20 @@ window.promises = [];
 
 // Do not change the code above this
 // add your promises to the array `promises`
+promises = Array.from({ length: 5 }, () => {
+  const randomDelay = Math.floor(Math.random() * 5000) + 1000; // Random time between 1 and 5 seconds
+  return new Promise((resolve) => {
+    setTimeout(() => {
+      resolve(`Promise resolved after ${randomDelay / 1000} seconds`);
+    }, randomDelay);
+  });
+});
+
+Promise.any(promises)
+  .then((result) => {
+    const outputDiv = document.getElementById("output");
+    outputDiv.textContent = result;
+  })
+  .catch((error) => {
+    console.error("All promises were rejected:", error);
+  });
